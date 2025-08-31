a0d1b6d7292864d4444de37be95d1fea1d9199ec
Jesy05 <jgonzalezj@uamv.edu.ni>
2025-08-31 16:11:05 -0600

    upd

---
+++ Ejercicios1raS/ProblemasSec3/probl1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Descripción: recorre los números del 1 al 100 con un bucle for, 
+// separa los pares e impares usando el operador módulo %, acumula 
+// cada grupo en variables distintas (sumapar y sumaimpar) y al final 
+// muestra en pantalla la suma total de los números pares y la suma total de los números impares.
+
+int sumapar = 0;
+int sumaimpar = 0;
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 2 == 0)
+    {
+        sumapar += i;
+    }
+    else
+    {
+        sumaimpar += i;
+    }
+}
+
+Console.WriteLine("Suma total de números pares: " + sumapar);
+Console.WriteLine("Suma total de números impares: " + sumaimpar);
---
+++ Ejercicios1raS/ProblemasSec3/probl2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Descripción: Escriba el programa que lea números del teclado mientras no se digite -99 
+// Determinar el mayor y el menor de los números  que fueron ingresados.
+
+int numero;
+int mayor = int.MinValue;
+int menor = int.MaxValue;
+
+Console.WriteLine("Ingrese números (-99 para terminar):");
+
+while (true)
+{
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero == -99)
+    {
+        break;
+    }
+
+    if (numero > mayor)
+    {
+        mayor = numero;
+    }
+
+    if (numero < menor)
+    {
+        menor = numero;
+    }
+}
+
+if (mayor != int.MinValue && menor != int.MaxValue)
+{
+    Console.WriteLine("El número mayor ingresado es: " + mayor);
+    Console.WriteLine("El número menor ingresado es: " + menor);
+}
+else
+{
+    Console.WriteLine("No se ingresaron números válidos.");
+}
