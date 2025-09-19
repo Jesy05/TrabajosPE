8c72db5893da3ade273b18efed1a87e61041f8e7
Jesy05 <jgonzalezj@uamv.edu.ni>
2025-09-19 10:47:47 -0600

    programas 1 2 de los primeros 4

---
+++ PracticaGruposde3/Funnciones/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static double CalcularAreaTriangulo(double baseT, double altura)
+    {
+        return (baseT * altura) / 2;
+    }
+
+    static void Main()
+    {
+        double area = CalcularAreaTriangulo(10, 5);
+        Console.WriteLine("Área del triángulo: " + area);
+    }
+}
---
+++ PracticaGruposde3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Intercambiar(ref int x, ref int y)
+    {
+        int temp = x;
+        x = y;
+        y = temp;
+    }
+
+    static void Main()
+    {
+        int a = 3, b = 7;
+        Console.WriteLine($"Antes: a={a}, b={b}");
+
+        Intercambiar(ref a, ref b);
+
+        Console.WriteLine($"Después: a={a}, b={b}");
+    }
+}
+
+
+ 
